696eeab4b9c1ed8cb9118c8c73af80fda7c91798
karmodaniel <daniel.carmo2008@gmail.com>
2020-08-31 03:50:33 -0400

    slide

---
+++ js/script.js
@@ -0,0 +1,15 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    nav: false,
+    auto: true,
+    time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    auto: true,
+    time: 5000,
+  });
+}
